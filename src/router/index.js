009db096d57ed9b33fb51632a0d37eff23c96cd3
luail <kynia0506@naver.com>
2025-02-24 10:23:43 +0900

    hotfix

--- src/router/index.js
+++ src/router/index.js
@@ -2,11 +2,17 @@ import{createRouter,createWebHistory} from 'vue-router';
 import { userRouter } from './userRouter';
 import { chatRouter } from './chatRouter';
 import { postRouter } from './postRouter';
+import HomePage from '@/views/HomePage.vue';
 
 const routes = [
     ...userRouter,
     ...postRouter,
     ...chatRouter,
+        {
+            path:'/ttt',
+            name:'HomePage',
+            component: HomePage
+        },
 ]
 
 
